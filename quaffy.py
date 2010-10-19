c403ac2e1ebb9db3d9f9b7ececc097ead69c3419
Nick Fisher <github@nickdafish.com>
2010-10-18 23:01:18 -0400

    cleaning up and implementing recursion

--- quaffy.py
+++ quaffy.py
@@ -2,26 +2,40 @@
 
 import paramiko
 import sqlite3
-import stat
+from stat import S_ISDIR
 from yaml import load
 from os.path import expanduser
+from pprint import pprint as pp
 
-cfg = load(open(expanduser('~/.quaffy')))
+def get_sftp(name='test'):
+    cfg = load(open(expanduser('~/.quaffy')))
+    host = cfg['test']['host']
+    port = cfg['test']['port']
+    user = cfg['test']['user']
+    passwd = cfg['test']['pass']
 
-host = cfg['test']['host']
-port = cfg['test']['port']
-user = cfg['test']['user']
-passwd = cfg['test']['pass']
+    transport = paramiko.Transport((host,port))
+    transport.connect(username=user, password=passwd)
+    return paramiko.SFTPClient.from_transport(transport)
 
-transport = paramiko.Transport((host,port))
-transport.connect(username=user, password=passwd)
-sftp = paramiko.SFTPClient.from_transport(transport)
-for f in sftp.listdir_attr():
-    print f.filename, stat.S_ISDIR(f.st_mode)
+def scan(sftp, path):
+    print " -> %s"%path
+    files = []
+    for f in sftp.listdir_attr(path):
+        if f.filename.startswith('.'): continue
+        if S_ISDIR(f.st_mode):
+            files.extend(scan(sftp, path+'/'+f.filename))
+        else:
+            files.append({
+                    'path': "/".join([path,f.filename]),
+                    'size': f.st_size,
+                    'mtime': f.st_mtime,
+                    })
+    return files
 
-
-# recursive func doing ls
-# [{path, size, m_time}, ...]
+if __name__ == '__main__':
+    sftp = get_sftp()
+    pp(scan(sftp, '.'))
 
 # cross check with db
 # if there is any size/m_time differnce - WARN
